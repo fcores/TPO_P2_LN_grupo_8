c1370c0109391789045284475f2adbd60f285834
juanantodb <111614538+juanantodb@users.noreply.github.com>
2023-06-27 12:49:59 -0300

    Update PilaDLimitCapacity.java

--- PilaDLimitCapacity.java
+++ PilaDLimitCapacity.java
@@ -12,54 +12,39 @@ public class PilaDinamicaLimitada implements PilaTDA {
         pila = new int[capacidad];
         cantidad = 0;
     }
-
-    /**
-     * @Costo: Constante
-     **/
+    
     public void InicializarPila() {
         cantidad = 0;
     }
 
-    /**
-     * @Costo: Constante (amortizado)
-     **/
     public void Apilar(int x) {
         if (cantidad < capacidad) {
             pila[cantidad] = x;
             cantidad++;
         } else {
-            // La pila está llena, se puede manejar un caso de error o lanzar una excepción
+            // Como la pila está vacía lo podemos considerar como un caso de error o una excepción
             System.out.println("Error: Pila llena, no se puede apilar el elemento " + x);
         }
     }
 
-    /**
-     * @Costo: Constante
-     **/
     public void Desapilar() {
         if (!PilaVacia()) {
             cantidad--;
         } else {
-            // La pila está vacía, se puede manejar un caso de error o lanzar una excepción
+            // Como la cola está vacía lo podemos considerar como un caso de error o una excepción
             System.out.println("Error: La pila está vacía, no se puede desapilar");
         }
     }
 
-    /**
-     * @Costo: Constante
-     **/
     public boolean PilaVacia() {
         return (cantidad == 0);
     }
-
-    /**
-     * @Costo: Constante
-     **/
+    
     public int Tope() {
         if (!PilaVacia()) {
             return pila[cantidad - 1];
         } else {
-            // La pila está vacía, se puede manejar un caso de error o lanzar una excepción
+            // Como la cola está vacía lo podemos considerar como un caso de error o una excepción
             System.out.println("Error: La pila está vacía, no tiene tope");
             return -1;
         }
